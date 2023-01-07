95198bf6e7f73a3eecb65596c28d141ba0988b08
siddharth00911 <siddharth00911@gmail.com>
2023-01-07 23:37:34 +0530

    f

--- closures.js
+++ closures.js
@@ -12,20 +12,52 @@
 // country();
 
 //////////////////////////////////
-// example 2
+// // example 2
 
-function z(){
-    var b=900;
-    function y(){
-        var a=7;
-        function x(){
-            console.log(a,b);
-        }
-        x();
-    }
-    y();
+// function z(){
+//     var b=900;
+//     function y(){
+//         var a=7;
+//         function x(){
+//             console.log(a,b);
+//         }
+//         x();
+//     }
+//     y();
+// }                 
+
+// z();
+/////////////////////////////////////////////////////////////////////
+
+//closer with setTimeout function
+
+// function x(){
+
+//     for(let i=1; i<=5; i++){
+//     setTimeout(function (){console.log(i);},  i * 3000  )                             //setTimeout it is callback fucntion
+//     }                
+    
+//     console.log('**********(hi-5)  Timer********');
+// }
+
+// x();
+//////////////////////////////////without using "let"
+function x(){
+
+    
+        for(var i=1; i<=5; i++){
+
+            function closure_fun(n){
+            setTimeout(function (){console.log(n);},  n * 1000  )    //setTimeout it is callback fucntion
+            }        
+            
+            closure_fun(i)
+            }    
+               
+    
+    console.log('**********(hi-5)  Timer********');
 }
 
-z();
+x();
 
 
