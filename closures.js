032f7d05412d569503fdc37ceb4d1adab21589c3
siddharth00911 <siddharth00911@gmail.com>
2023-01-08 23:01:58 +0530

    g

--- closures.js
+++ closures.js
@@ -42,22 +42,114 @@
 
 // x();
 //////////////////////////////////without using "let"
-function x(){
+// function x(){
 
     
-        for(var i=1; i<=5; i++){
+//         for(var i=1; i<=5; i++){
 
-            function closure_fun(n){
-            setTimeout(function (){console.log(n);},  n * 1000  )    //setTimeout it is callback fucntion
-            }        
+//             function closure_fun(n){
+//             setTimeout(function (){console.log(n);},  n * 1000  )    //setTimeout it is callback fucntion
+//             }        
             
-            closure_fun(i)
-            }    
+//             closure_fun(i)
+//             }    
                
     
-    console.log('**********(hi-5)  Timer********');
+//     console.log('**********(hi-5)  Timer********');
+// }
+
+// x();
+///////////////////////////////////////////////////////////////
+
+//closure interview
+
+// // 1.
+// function outer(){
+
+//     let a=5;
+//     return function inner(){
+//         console.log(a);
+//     }
+// }
+
+// outer()();
+
+// let o1= outer();
+// o1();
+
+
+// // 2.
+// function outer(x){
+//     let a=5;
+//     return function inner(){
+//         console.log(a, x);
+//     }
+
+
+// }
+
+// outer("hi-5")();
+
+
+// // // 3.
+// function outerest(){
+//     let b=10;
+//     function outer(){ 
+//         let a=5;
+//         return function inner(){
+//             console.log(a, b, c);
+//         }
+//     }
+//     return outer;
+// }
+
+// let c=15;
+
+// let o1=outerest()();
+// o1();
+
+// // // 4.
+// function outerest(){
+//     function outer(b){ 
+//         let a=5;
+//         return function inner(){
+//             console.log(a, b, c);
+//         }
+//     }
+//     return outer;
+// }
+
+// let c=15;
+
+// let o1=outerest()('hi-5');
+// o1();
+
+//////////////////////////////
+
+// advantages ->
+// 1. module Pattern
+// 2. currying
+// 3. in some higher order functions like memoize, once
+// 4. it help in data hiding and encapsulations
+
+
+// data hinding/encapsulations-> hiding some-functions/variables in programs
+// eg.1
+
+function Counter(){
+    let count=1;
+    this.ICounter = function(){
+        count++;
+        console.log(count);
+    }
+    this.DCounter = function(){
+        count--;
+        console.log(count);
+    }
 }
 
-x();
+let counter1 = new Counter();
 
-
+counter1.ICounter();
+counter1.ICounter();
+counter1.DCounter();
