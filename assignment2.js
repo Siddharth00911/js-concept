57b61cf921594c5b848ccbc33f40a3bee3548229
siddharth00911 <siddharth00911@gmail.com>
2023-01-13 14:46:53 +0530

    l

---
+++ assignment2.js
@@ -0,0 +1,16 @@
+//to check fabbonacci series
+
+let n=5;
+let count=1;
+
+    for(let i=1; i<=n; i++){
+        count=count*i;
+    }
+console.log(count);
+   
+
+// count= 1 * 1        -> 1
+// count= 1 * 2        -> 2
+// count= 2 * 3        -> 6
+// count= 6 * 4        -> 24
+// count= 24 * 5       -> 120
---
+++ assignment3.js
@@ -0,0 +1,6 @@
+//to find largest number
+
+let arr=[1,4,6, 8, 2, 3, 5, 7, 9];
+
+console.log(arr.sort());
+console.log(arr.reverse());
